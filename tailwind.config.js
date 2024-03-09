4953576f92e9c8be35445aebd984721c823af7f9
Shivam12202945 <nowmailshivamkumar@gmail.com>
2024-03-09 21:16:23 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+  
+  colors:{
+    deepBlue:"#02042a",
+    lightBlue:"2b84ea",
+    lightBlue300:"#4b94ed",
+  },  
+    }, 
+},
+
+  plugins: [],
+}
+
